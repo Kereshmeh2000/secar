cc5d304c3534763d7a99aa5172f64c926affa735
Kereshmeh <kereshmehsaadati@gmail.com>
2024-02-12 20:59:16 +0330

    responsive menu added

---
+++ main/js/responsive-menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("side-navigation-drawer").style.width = "300px";
+}
+
+function closeMenu() {
+    document.getElementById("side-navigation-drawer").style.width = "0";
+}
